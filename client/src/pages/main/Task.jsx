9894d0731ecff94763a3327004b946151703e87c
itsZeroFour <itsZeroFourX@gmail.com>
2025-09-03 16:57:45 +0300

    update

--- client/src/pages/main/Task.jsx
+++ client/src/pages/main/Task.jsx
@@ -5,129 +5,164 @@ import useDisableScroll from "../../hooks/useDisableScroll";
 import useUser from "../../hooks/useUser";
 import useVKAuth from "../../hooks/useVKAuth";
 import TaskPopup from "../../components/task-popup/TaskPopup";
+import useCommentStatus from "../../hooks/useCommentStatus";
+import useSubscriptionStatus from "../../hooks/useSubscriptionStatus";
+import useRepostStatus from "../../hooks/useRepostStatus";
+import TargetActionsPopup from "../../components/target-actions-popup/TargetActionsPopup";
 
-const Task = React.memo(
-  ({ isSubscribe, isCommented, isShared, user, finalUserId }) => {
-    const [showPopup, setShowPopup] = useState(false);
-    const [navigateItemClick, setNavigateItemClick] = useState("");
-    const navigate = useNavigate();
+const Task = React.memo(({ user, finalUserId }) => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [navigateItemClick, setNavigateItemClick] = useState("");
+  const [openTargetActionModel, setOpenTargetActionModel] = useState("");
+  const navigate = useNavigate();
 
-    const { userId, userData } = useUser();
-    const { accessToken } = useVKAuth(userId);
+  const { userId, userData } = useUser();
+  const { accessToken } = useVKAuth(userId);
 
-    const items = [
-      {
-        bg: "/images/main/tasks/bg/bg-1.webp",
-        img: "/images/main/tasks/task-1.webp",
-        isDone: user?.gamesComplete?.first_game,
-        title: "игра \\ «распознавание лиц»",
-        path: "/menu/hub/game1",
-      },
-      {
-        bg: "/images/main/tasks/bg/bg-2.webp",
-        img: "/images/main/tasks/task-2.webp",
-        isDone: user?.gamesComplete?.second_game,
-        title: "игра \\ «СВОЙ-ЧУЖОЙ»",
-        path: "/menu/hub/game3",
-      },
-      {
-        bg: "/images/main/tasks/bg/bg-3.webp",
-        img: "/images/main/tasks/task-3.webp",
-        isDone: user?.gamesComplete?.third_game,
-        title: "игра \\ «ТОЧКИ контакта»",
-        path: "/menu/hub/game2",
-      },
-      {
-        bg: "/images/main/tasks/bg/bg-4.webp",
-        img: "/images/main/tasks/task-4.png",
-        isDone: isCommented,
-        title: "вопрос \\ на засыпку",
-        path: "/menu/hub/game2",
-      },
-      {
-        bg: "/images/main/tasks/bg/bg-5.webp",
-        img: "/images/main/tasks/task-5.png",
-        isDone: isSubscribe,
-        title: "подписка на паблик",
-        path: "/menu/hub/game2",
-      },
-      {
-        bg: "/images/main/tasks/bg/bg-6.webp",
-        img: "/images/main/tasks/task-6.png",
-        isDone: isShared,
-        title: "репост записи в группе",
-        path: "/menu/hub/game2",
-      },
-    ];
+  const isCommented = useCommentStatus(accessToken, finalUserId, user);
+  const isSubscribe = useSubscriptionStatus(accessToken, finalUserId, user);
+  const isShared = useRepostStatus(accessToken, finalUserId, user);
 
-    const handleItemClick = (item, e) => {
-      e.preventDefault();
+  const items = [
+    {
+      bg: "/images/main/tasks/bg/bg-1.webp",
+      img: "/images/main/tasks/task-1.webp",
+      isDone: user?.gamesComplete?.first_game,
+      title: "игра \\ «распознавание лиц»",
+      path: "/menu/hub/game1",
+    },
+    {
+      bg: "/images/main/tasks/bg/bg-2.webp",
+      img: "/images/main/tasks/task-2.webp",
+      isDone: user?.gamesComplete?.second_game,
+      title: "игра \\ «СВОЙ-ЧУЖОЙ»",
+      path: "/menu/hub/game3",
+    },
+    {
+      bg: "/images/main/tasks/bg/bg-3.webp",
+      img: "/images/main/tasks/task-3.webp",
+      isDone: user?.gamesComplete?.third_game,
+      title: "игра \\ «ТОЧКИ контакта»",
+      path: "/menu/hub/game2",
+    },
+    {
+      bg: "/images/main/tasks/bg/bg-4.webp",
+      img: "/images/main/tasks/task-4.png",
+      isDone: isCommented,
+      title: "вопрос \\ на засыпку",
+      path: "https://vk.com/club232395157?from=groups&w=wall-232395157_1",
+    },
+    {
+      bg: "/images/main/tasks/bg/bg-5.webp",
+      img: "/images/main/tasks/task-5.png",
+      isDone: isSubscribe,
+      title: "подписка на паблик",
+      path: "https://vk.com/club232395157",
+    },
+    {
+      bg: "/images/main/tasks/bg/bg-6.webp",
+      img: "/images/main/tasks/task-6.png",
+      isDone: isShared,
+      title: "репост записи в группе",
+      path: "https://vk.com/club232395157?from=groups&w=wall-232395157_2",
+    },
+  ];
 
-      if (finalUserId) {
-        navigate(item.path);
-      } else {
-        setShowPopup(true);
-      }
-    };
+  const handleItemClick = (item, e) => {
+    e.preventDefault();
 
-    useDisableScroll(showPopup);
+    if (finalUserId) {
+      navigate(item.path);
+    } else {
+      setShowPopup(true);
+    }
+  };
 
-    const continueOnClick = () => {
-      navigate(navigateItemClick);
-    };
+  useDisableScroll(showPopup);
 
-    return (
-      <section className={style.task}>
-        <TaskPopup
-          showPopup={showPopup}
-          onClose={() => setShowPopup(false)}
+  const continueOnClick = () => {
+    navigate(navigateItemClick);
+  };
+
+  return (
+    <section className={style.task}>
+      <TaskPopup
+        showPopup={showPopup}
+        onClose={() => setShowPopup(false)}
+        finalUserId={finalUserId}
+        userId={userId}
+        accessToken={accessToken}
+        userData={userData}
+        continueOnClick={continueOnClick}
+      />
+
+      {openTargetActionModel && (
+        <TargetActionsPopup
+          targetAction={openTargetActionModel}
+          isSubscribe={isSubscribe}
+          isCommented={isCommented}
+          isShared={isShared}
           finalUserId={finalUserId}
-          userId={userId}
+          userData={user}
           accessToken={accessToken}
-          userData={userData}
-          continueOnClick={continueOnClick}
+          setOpenTargetActionModel={setOpenTargetActionModel}
+          link={
+            openTargetActionModel === "subscribe"
+              ? items[4].path
+              : openTargetActionModel === "comment"
+              ? items[3].path
+              : items[5].path
+          }
         />
+      )}
 
-        <div className="container">
-          <div className={style.task__wrapper}>
-            <ul>
-              {items.map(({ bg, img, isDone, title, path }) => (
-                <li
-                  key={title}
-                  style={{
-                    background: `url(${bg})`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    cursor: "pointer",
-                  }}
-                  onClick={(e) => {
+      <div className="container">
+        <div className={style.task__wrapper}>
+          <ul>
+            {items.map(({ bg, img, isDone, title, path }, index) => (
+              <li
+                key={title}
+                style={{
+                  background: `url(${bg})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  cursor: "pointer",
+                }}
+                onClick={(e) => {
+                  if (index < 3) {
                     setNavigateItemClick(path);
                     handleItemClick({ path }, e);
-                  }}
-                >
-                  <div className={style.task__top}>
-                    <p
-                      style={
-                        isDone ? { color: "#1BDB65" } : { color: "#A6A9A6" }
-                      }
-                    >
-                      {isDone ? "Выполнено" : "Не выполнено"}
-                    </p>
-                    <img src={img} alt={title} />
-                  </div>
+                  } else {
+                    setOpenTargetActionModel(
+                      index === 3
+                        ? "comment"
+                        : index === 4
+                        ? "subscribe"
+                        : "share"
+                    );
+                  }
+                }}
+              >
+                <div className={style.task__top}>
+                  <p
+                    style={isDone ? { color: "#1BDB65" } : { color: "#A6A9A6" }}
+                  >
+                    {isDone ? "Выполнено" : "Не выполнено"}
+                  </p>
+                  <img src={img} alt={title} />
+                </div>
 
-                  <h3 style={{ whiteSpace: "pre-line" }}>
-                    {title.replace(/\\/g, "\n")}
-                  </h3>
-                </li>
-              ))}
-            </ul>
-          </div>
+                <h3 style={{ whiteSpace: "pre-line" }}>
+                  {title.replace(/\\/g, "\n")}
+                </h3>
+              </li>
+            ))}
+          </ul>
         </div>
-      </section>
-    );
-  }
-);
+      </div>
+    </section>
+  );
+});
 
 export default Task;
